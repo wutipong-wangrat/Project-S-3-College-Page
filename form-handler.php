b42c81bb3ed1be2b054014d45d26c1ce3c35876c
wutipong wangrat <bigbosszaa086@gmail.com>
2023-01-29 19:39:06 +0700

    Hi

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_form = 'project-s-3-college-page.vercel.app';
+$email_form = 'Email1';
 
 $email_subject = 'new Form Submission';
 
