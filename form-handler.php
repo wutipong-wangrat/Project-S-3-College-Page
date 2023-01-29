e82c79a8ca29cbfa9aa2b3ca2903400cf877ad88
wutipong wangrat <bigbosszaa086@gmail.com>
2023-01-29 19:27:49 +0700

    Hi

---
+++ form-handler.php
@@ -0,0 +1,26 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form = 'project-s-3-college-page.vercel.app';
+
+$email_subject = 'new Form Submission';
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                 "Subject: $subject.\n".
+                  "User Message: $message.\n";
+
+$to = 'bigbosszaa086@gamil.com';
+
+$headers = "From: $email_form \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+?>
